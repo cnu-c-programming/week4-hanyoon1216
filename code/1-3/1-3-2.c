973c1b83f64d738f3faabc66954418442a7f2c0d
hanyoon1216 <hanyoon1216@gmail.com>
2026-03-27 19:36:43 +0900

    Update 1-3-2.c

--- code/1-3/1-3-2.c
+++ code/1-3/1-3-2.c
@@ -4,12 +4,12 @@
 
 void logger_global(char* message) {
     static int count = 0;
-    printf("log %d: %s\n", ++count, message);
+    printf("log %2d: %s\n", ++count, message);
 }
 
 void task2() {
     for(int i = 0; i < 4; i++) {
-        logger_static_inline("task 2");
-        logger_global("task 2");
+        logger_static_inline("task2");
+        logger_global("task2");
     }
 }
