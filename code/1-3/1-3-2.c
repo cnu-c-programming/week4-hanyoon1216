efb4b3c30dfce33ad53d2b91503a189d4fc8eb89
hanyoon1216 <hanyoon1216@gmail.com>
2026-03-27 19:28:33 +0900

    Update 1-3-2.c

---
+++ code/1-3/1-3-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "util.h"
+
+void logger_global(char* message) {
+    static int count = 0;
+    printf("log %d: %s\n", ++count, message);
+}
+
+void task2() {
+    for(int i = 0; i < 5; i++) {
+        logger_static_inline("task 2");
+        logger_global("task 2");
+    }
+}
